7054bdb6265b142ee202417ae023912b1ffa55bf
shadow-rc <animegamer371524@gmail.com>
2026-05-13 00:54:26 +0530

    also added binary search

--- rough.cpp
+++ rough.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 void Linearsearch(int arr[],int key,int n){
   for(int i=0;i<n;i++){
     if(arr[i]==key){
@@ -12,10 +11,25 @@ void Linearsearch(int arr[],int key,int n){
   cout << "not found" << endl;
 }
 
+void binarysearch(int arr[],int low,int high,int key,int n){
+  int mid=low + (high - low )/2;
+  if(arr[mid]==key){
+    cout<<"found at mid "<<mid<<endl;
+    return;
+  if(arr[mid]<key){
+    mid = low + 1; 
+  }
+  if (arr[mid]>key){
+    mid = high - 1;
+  }
+  }
+}
+
 
 int main (){
   int arr[]={10,20,30,45,55,60};
   int key=45;
   int n=6;
   Linearsearch(arr,key,n);
+  binarysearch(arr,0,5,30,6);
 }
